e90c943907c11aaaa06766f231799e22326e5338
telegrammae <telegrammae@gmail.com>
2019-09-25 22:11:24 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,205 @@
+const emojis = { wolf: '🐺', goat: '🐐', cabbage: '🥦', traveler: '🚣' };
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.updateSides = this.updateSides.bind(this);
+        this.handleRestart = this.handleRestart.bind(this);
+        this.check = this.check.bind(this);
+        this.state = {
+            gameOver: false,
+            currentSide: 'left',
+            left: ['wolf', 'goat', 'cabbage', 'traveler'],
+            right: []
+        };
+    }
+
+    arrayDifference(a, b) {
+        return a.filter(x => !b.includes(x));
+    }
+
+    handleRestart() {
+        this.setState(() => {
+            return {
+                gameOver: false,
+                reason: '',
+                currentSide: 'left',
+                left: ['wolf', 'goat', 'cabbage', 'traveler'],
+                right: []
+            };
+        });
+    }
+
+    check() {
+        const abandoned = this.state.currentSide === 'left' ? 'right' : 'left';
+        const bank = this.state[abandoned];
+        if (bank.includes('wolf') && bank.includes('goat')) {
+            this.setState(() => {
+                return { gameOver: true, reason: 'The Wolf ate the Goat!' };
+            });
+        }
+        if (bank.includes('cabbage') && bank.includes('goat')) {
+            this.setState(() => {
+                return {
+                    gameOver: true,
+                    reason: 'The Goat ate the Cabbage!'
+                };
+            });
+        }
+        if (this.state.right.length === 4) {
+            this.setState(() => {
+                return {
+                    gameOver: true,
+                    reason: 'Nice! All have crossed the river safely!'
+                };
+            });
+        }
+    }
+
+    updateSides(chosen) {
+        this.setState(
+            prevState => {
+                return {
+                    left:
+                        prevState.currentSide === 'left'
+                            ? this.arrayDifference(prevState.left, chosen)
+                            : chosen.concat(prevState.left),
+                    right:
+                        prevState.currentSide === 'left'
+                            ? chosen.concat(prevState.right)
+                            : this.arrayDifference(prevState.right, chosen),
+                    currentSide:
+                        prevState.currentSide === 'left' ? 'right' : 'left'
+                };
+            },
+            () => {
+                this.check();
+            }
+        );
+    }
+
+    show() {
+        if (this.state.gameOver) {
+            return <Victory handleRestart={this.handleRestart} />;
+        } else if (this.state.loss) {
+            return <Loss handleRestart={this.handleRestart} />;
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.gameOver ? (
+                    <GameOver
+                        reason={this.state.reason}
+                        handleRestart={this.handleRestart}
+                    />
+                ) : (
+                    <div>
+                        <h2 id="title">Wolf, Goat, and Cabbage</h2>
+                        <Side
+                            updateSides={this.updateSides}
+                            location="left"
+                            items={this.state.left}
+                            currentSide={this.state.currentSide}
+                        />
+                        <Side
+                            updateSides={this.updateSides}
+                            location="right"
+                            items={this.state.right}
+                            currentSide={this.state.currentSide}
+                        />
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+function GameOver(props) {
+    return (
+        <div id="victory">
+            <h1>Game Over!</h1>
+            <p>{props.reason}</p>
+            <button onClick={props.handleRestart}>Play again</button>
+        </div>
+    );
+}
+
+class Side extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSideState = this.handleSideState.bind(this);
+        this.handleChoose = this.handleChoose.bind(this);
+        this.handleChoose = this.handleChoose.bind(this);
+        this.clearError = this.clearError.bind(this);
+        this.state = { error: undefined };
+    }
+
+    handleChoose(e) {
+        e.preventDefault();
+        const chosen = Object.values(e.target.elements)
+            .filter(el => el.checked)
+            .map(el => el.value);
+        const error = this.handleSideState(chosen);
+        this.setState(() => {
+            return { error };
+        });
+    }
+
+    handleSideState(chosen) {
+        if (!chosen.includes('traveler')) {
+            return 'Error: traveler must always be selected';
+        } else if (chosen.length > 2) {
+            return 'Error: can move at most one thing at once';
+        }
+        this.props.updateSides(chosen);
+    }
+
+    clearError() {
+        if (this.state.error) {
+            this.setState(() => {
+                return { error: undefined };
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="side" id={this.props.location}>
+                <h3>{`${this.props.location} side`.toUpperCase()}</h3>
+                <div>
+                    {this.state.error && <p>{this.state.error}</p>}
+                    <form
+                        onSubmit={this.handleChoose}
+                        onChange={this.clearError}>
+                        {this.props.items.map(item => (
+                            <div key={item}>
+                                <input
+                                    type="checkbox"
+                                    name={item}
+                                    value={item}
+                                    disabled={
+                                        this.props.currentSide !==
+                                        this.props.location
+                                    }
+                                    defaultChecked={item === 'traveler'}
+                                />
+                                <label>{emojis[item] + item}</label>
+                            </div>
+                        ))}
+                        <button
+                            disabled={
+                                this.props.currentSide !== this.props.location
+                            }>
+                            Move{' '}
+                            {this.props.location === 'left' ? 'right' : 'left'}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById('app'));
