9794a7cceb77b9c9f01d6cb2a6568098de1fc88f
telegrammae <telegrammae@gmail.com>
2019-09-28 23:44:03 -0400

    split out main JS file

---
+++ src/components/Game.js
@@ -0,0 +1,117 @@
+import React from 'react';
+
+import Side from './Side';
+
+export default class Game extends React.Component {
+    state = {
+        gameOver: false,
+        currentSide: 'left',
+        left: ['wolf', 'goat', 'cabbage', 'traveler'],
+        right: []
+    };
+
+    arrayDifference = (a, b) => {
+        return a.filter(x => !b.includes(x));
+    };
+
+    handleRestart = () => {
+        this.setState(() => {
+            return {
+                gameOver: false,
+                reason: '',
+                currentSide: 'left',
+                left: ['wolf', 'goat', 'cabbage', 'traveler'],
+                right: []
+            };
+        });
+    };
+
+    check = () => {
+        const abandoned = this.state.currentSide === 'left' ? 'right' : 'left';
+        const bank = this.state[abandoned];
+        if (bank.includes('wolf') && bank.includes('goat')) {
+            this.setState(() => {
+                return { gameOver: true, reason: 'The Wolf ate the Goat!' };
+            });
+        }
+        if (bank.includes('cabbage') && bank.includes('goat')) {
+            this.setState(() => {
+                return {
+                    gameOver: true,
+                    reason: 'The Goat ate the Cabbage!'
+                };
+            });
+        }
+        if (this.state.right.length === 4) {
+            this.setState(() => {
+                return {
+                    gameOver: true,
+                    reason: 'Nice! All have crossed the river safely!'
+                };
+            });
+        }
+    };
+
+    updateSides = chosen => {
+        this.setState(
+            prevState => {
+                return {
+                    left:
+                        prevState.currentSide === 'left'
+                            ? this.arrayDifference(prevState.left, chosen)
+                            : chosen.concat(prevState.left),
+                    right:
+                        prevState.currentSide === 'left'
+                            ? chosen.concat(prevState.right)
+                            : this.arrayDifference(prevState.right, chosen),
+                    currentSide:
+                        prevState.currentSide === 'left' ? 'right' : 'left'
+                };
+            },
+            () => {
+                this.check();
+            }
+        );
+    };
+
+    render() {
+        return (
+            <div>
+                {this.state.gameOver ? (
+                    <GameOver
+                        reason={this.state.reason}
+                        handleRestart={this.handleRestart}
+                    />
+                ) : (
+                    <div>
+                        <h2 id="title">Wolf, Goat, and Cabbage</h2>
+                        <div>
+                            <Side
+                                updateSides={this.updateSides}
+                                location="left"
+                                items={this.state.left}
+                                currentSide={this.state.currentSide}
+                            />
+                            <Side
+                                updateSides={this.updateSides}
+                                location="right"
+                                items={this.state.right}
+                                currentSide={this.state.currentSide}
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
+
+const GameOver = props => {
+    return (
+        <div id="victory">
+            <h1>Game Over!</h1>
+            <p>{props.reason}</p>
+            <button onClick={props.handleRestart}>Play again</button>
+        </div>
+    );
+};
